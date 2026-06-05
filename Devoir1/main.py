43b47df1196ef5a744bcc2dd2aca88111871debd
Charles Ally <charles.ally04@gmail.com>
2026-06-05 18:32:19 -0400

    Algorithme de correction de la perspective basé sur le parallélisme

--- Devoir1/main.py
+++ Devoir1/main.py
@@ -1,16 +1,54 @@
-# This is a sample Python script.
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Récupération de l'image
+image = cv2.imread("Sol.png")
+hauteur, largeur, _ = image.shape
 
+# Points des droites parallèles
+p1 = np.array([45, 128, 1])
+p2 = np.array([258, 49, 1])
+p3 = np.array([211, 266, 1])
+p4 = np.array([440, 141, 1])
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Recherche du premier point de fuite
+d1 = np.cross(p1, p2)
+d2 = np.cross(p3, p4)
+f1 = np.cross(d1, d2)
 
+# Recherche du deuxième point de fuite
+d3 = np.cross(p1, p3)
+d4 = np.cross(p2, p4)
+f2 = np.cross(d3, d4)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Calcul de la ligne d'horizon
+d = np.cross(f1, f2)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Normalisation de la ligne d'horizon
+# En géométrie projective, d et lambda*d représentent la même droite.
+# OpenCV applique la transformation via une division cartésienne.
+# Sans cette normalisation, les valeurs de d sont trop grandes,
+# ce qui écrase toutes les coordonnées vers (0,0) et produit une image de sortie noire.
+# À l'inverse, des valeurs trop petites donneraient une image géante.
+d = d / d[2]
+
+# Matrice de transformation H2
+H2 = np.array([
+    [1, 0, 0],
+    [0, 1, 0],
+    [d[0], d[1], d[2]]
+])
+
+# Application de la matrice à toute l'image
+image_rectifiee = cv2.warpPerspective(image, H2, (largeur, hauteur))
+
+plt.figure(figsize=(10, 5))
+
+plt.imshow(image)
+plt.axis('off')
+plt.show()
+plt.imshow(image_rectifiee)
+plt.axis('off')
+plt.savefig("rectifiee.jpg")
+plt.show()
